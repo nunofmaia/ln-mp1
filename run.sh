1ddcbcc7f74694280eda8199d1dedc252dca445f
Nuno Maia <nunofmaia@gmail.com>
2013-10-03 10:44:12 +0100

    Add compose function to the script for a suffix. Add more states and transitions to trad_isco.txt

--- run.sh
+++ run.sh
@@ -13,3 +13,11 @@ do
 done
 
 # Compose word transducers with suffix tranducers
+fsmcompose namorisco.fsm trad_isco.fsm > namoro.fsm
+fsmdraw -i letras.syms -o letras.syms namoro.fsm | dot -Tpdf > namoro.pdf
+
+fsmcompose mourisco.fsm trad_isco.fsm > mouro.fsm
+fsmdraw -i letras.syms -o letras.syms mouro.fsm | dot -Tpdf > mouro.pdf
+
+fsmcompose chuvisco.fsm trad_isco.fsm > chuva.fsm
+fsmdraw -i letras.syms -o letras.syms chuva.fsm | dot -Tpdf > chuva.pdf
